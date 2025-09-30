f3d67e5b5fc3626147b71861ecd8ef0dd2eb41e4
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2025-09-30 14:10:13 +0300

    Send context back to requester

--- functions/entities.js
+++ functions/entities.js
@@ -1,10 +1,8 @@
 export async function onRequestGet(context) {
-    const { pathname: url } = new URL(context.request.url);
-
     const { results } = await context.env.test_d1
       .prepare("SELECT * FROM Entities")
       .run();
 
-    const result = { pathname: url, params: url.searchParams, results: results };
+    const result = { ctx: context, results: results };
     return Response.json(result);
 };
