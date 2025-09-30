db8dfbd63aebf0d25b9ffb495664be78887e815b
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2025-09-30 15:28:22 +0300

    Refactor variable to reflect the role as sql response

--- functions/entities.js
+++ functions/entities.js
@@ -4,17 +4,17 @@ export async function onRequestGet(context) {
 	const url = new URL(context.request.url);
 	const param = url.searchParams.get("id");
 
-	let results = null;
+	let sqlResponse = null;
 	if(param) {
-		results = await context.env.test_d1
+		sqlResponse = await context.env.test_d1
 			.prepare("SELECT * FROM Entities where ID = ?")
 			.bind(param)
 			.run();
 	} else {
-		results = await context.env.test_d1
+		sqlResponse = await context.env.test_d1
 			.prepare("SELECT * FROM Entities")
 			.run();
 	}
 
-	return Response.json(results.results);
+	return Response.json(sqlResponse.results);
 };
