49b705890e5cc4a282524abca67a71655ee32878
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2025-09-30 12:53:30 +0300

    Fix entities syntax

--- functions/entities.js
+++ functions/entities.js
@@ -1,12 +1,9 @@
-export default {
-  async fetch(request, env) {
-    const { pathname } = new URL(request.url);
+export async function onRequest(context) {
+    const { pathname } = new URL(context.request.url);
 
-    console.log("Pathname:", pathname);
-    // If you did not use `DB` as your binding name, change it here
-    const { results } = await env.test_d1
-    .prepare("SELECT * FROM Entities")
-    .run();
+    alert("Pathname:", pathname);
+    const { results } = await context.env.test_d1
+      .prepare("SELECT * FROM Entities")
+      .run();
     return Response.json(results);
-  },
 };
