8b7c44e6739689b1ac96042ff0fb59495d8442b2
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2025-09-30 14:26:18 +0300

    Added param binding to sql query

--- functions/entities.js
+++ functions/entities.js
@@ -1,10 +1,20 @@
 export async function onRequestGet(context) {
-	const { results } = await context.env.test_d1
-		.prepare("SELECT * FROM Entities")
-		.run();
+	
 
 	const url = new URL(context.request.url);
-	const param = url.searchParams.get("param");
-	const result = { url: url, params: param, results: results };
-	return Response.json(result);
+	const param = url.searchParams.get("id");
+
+	let results = null;
+	if(param) {
+		results = await context.env.test_d1
+			.prepare("SELECT * FROM Entities where ID = ?")
+			.bind(param)
+			.run();
+	} else {
+		results = await context.env.test_d1
+			.prepare("SELECT * FROM Entities")
+			.run();
+	}
+
+	return Response.json({ results });
 };
