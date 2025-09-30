d611412848b3019ecd485af71b49e0db6d3043a2
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2025-09-30 14:29:36 +0300

    Extract results from sql response

--- functions/entities.js
+++ functions/entities.js
@@ -4,17 +4,19 @@ export async function onRequestGet(context) {
 	const url = new URL(context.request.url);
 	const param = url.searchParams.get("id");
 
-	let results = null;
+	let entities = null;
 	if(param) {
-		results = await context.env.test_d1
+		entities = await context.env.test_d1
 			.prepare("SELECT * FROM Entities where ID = ?")
 			.bind(param)
-			.run();
+			.run()
+			.results;
 	} else {
-		results = await context.env.test_d1
+		entities = await context.env.test_d1
 			.prepare("SELECT * FROM Entities")
-			.run();
+			.run()
+			.results;
 	}
 
-	return Response.json({ results });
+	return Response.json(entities);
 };
