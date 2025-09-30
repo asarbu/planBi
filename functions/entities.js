7c456443de94934cdb223de58ec7fc7d7bdf0556
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2025-09-30 13:36:27 +0300

    Added params alert to entities request

--- functions/entities.js
+++ functions/entities.js
@@ -2,6 +2,8 @@ export async function onRequest(context) {
     const { pathname } = new URL(context.request.url);
 
     alert("Pathname:", pathname);
+    if(context.params) alert("Params:", context.params);
+
     const { results } = await context.env.test_d1
       .prepare("SELECT * FROM Entities")
       .run();
