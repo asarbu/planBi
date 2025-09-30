9eea35fd39027c7d632afc7761ef76f729540680
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2025-09-30 14:18:32 +0300

    Explicitly get param from url

--- functions/entities.js
+++ functions/entities.js
@@ -1,8 +1,10 @@
 export async function onRequestGet(context) {
-    const { results } = await context.env.test_d1
-      .prepare("SELECT * FROM Entities")
-      .run();
+	const { results } = await context.env.test_d1
+		.prepare("SELECT * FROM Entities")
+		.run();
 
-    const result = { ctx: context, results: results };
-    return Response.json(result);
+	const url = new URL(context.request.url);
+	const param = url.searchParams.get("param");
+	const result = { url: url, params: param, results: results };
+	return Response.json(result);
 };
