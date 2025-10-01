c67374c32bede127fa99efa433f6381a30c233ff
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2025-10-01 10:59:21 +0300

    Debug formData

--- functions/meetings.js
+++ functions/meetings.js
@@ -1,11 +1,11 @@
 export async function onRequestPost(context) {
 	const url = new URL(context.request.url);
+	//const param = url.searchParams.get("id");
     const formData = await context.request.formData();
-    const fullName = formData.get("fullName");
+    /*const fullName = formData.get("fullName");
     const email = formData.get("email");
     const county = formData.get("county");
-    const telephone = formData.get("telephone");
-	const param = url.searchParams.get("id");
+    const telephone = formData.get("telephone");*/
 
-	return Response.json({ fullName, email, county, telephone, success: true });
+	return Response.json({ formData: JSON.stringify(formData) });
 };
