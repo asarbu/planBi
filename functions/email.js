08a9845aef2ee63f5fc67801e7dda88f16f3054c
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2025-10-20 13:28:10 +0300

    Add email functionality

---
+++ functions/email.js
@@ -0,0 +1,18 @@
+import { Resend } from 'resend';
+export async function onRequestPost(context) {
+  try {
+    // Using text instead of email so that I don't need to sanitize it
+    const resend = new Resend(context.env.RESEND_API_KEY);
+    const { data, error } = await resend.emails.send({
+      from: "contact@planbi.ro",
+      reply_to: 'output@email.ro',
+      to: "sarbu_alexandru_92@yahoo.com",
+      subject: `This is a test`,
+      text: "This is a test email sent.",
+    });
+    console.log({data, error});
+    return Response.json({ data: data, error: error });
+  } catch (err) {
+    return Response.json({ exception: err });
+  }
+}
