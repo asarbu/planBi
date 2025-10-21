fd6be4b8f00d3927e7ffa7ed73aa147466f7e6f2
Alexandru-Sebastian SARBU <sarbu_alexandru_92@yahoo.com>
2025-10-21 06:00:28 +0300

    Enhance error handling for email

--- functions/email.js
+++ functions/email.js
@@ -1,44 +1,40 @@
 import { Resend } from 'resend';
 export async function onRequestPost(context) {
 	try {
-		const requestBody = await context.request.json();
-		if (!requestBody.name
-			|| !requestBody.email
-			|| !requestBody.telephone
-			|| !requestBody.service_type
-			|| !requestBody.date
-			|| !requestBody.timeslot) {
-				const missingFields = [];
-				if (!requestBody.name) missingFields.push('name');
-				if (!requestBody.email) missingFields.push('email');
-				if (!requestBody.telephone) missingFields.push('telephone');
-				if (!requestBody.service_type) missingFields.push('service_type');
-				if (!requestBody.date) missingFields.push('date');
-				if (!requestBody.timeslot) missingFields.push('timeslot');
-				return Response.json({ error: 'Missing required fields', missingFields }, { status: 400 });
-		}
+		   const requestBody = await context.request.json();
+		   const requiredFields = ['name', 'email', 'telephone', 'service_type', 'date', 'timeslot'];
+		   const missingFields = requiredFields.filter(f => !requestBody[f]);
+		   if (missingFields.length) {
+			   return Response.json({ error: 'Campuri lipsa', missingFields }, { status: 400 });
+		   }
 
 		const messageBody = `
-			Name: ${requestBody.name}
+			Nume: ${requestBody.name}
 			Email: ${requestBody.email}
-			Telephone: ${requestBody.telephone}
-			Service Type: ${requestBody.service_type}
-			Date: ${requestBody.date}
-			Timeslot: ${requestBody.timeslot}
-			Message: ${requestBody.message || ''}
+			Telefon: ${requestBody.telephone}
+			Tip Serviciu: ${requestBody.service_type}
+			Dată: ${requestBody.date}
+			Fereastră: ${requestBody.timeslot}
+			Mesaj: ${requestBody.message || ''}
 			`;
 
+		//TODO Replace with fetch api. Remove build deps, clear build command in dashboard
 		const resend = new Resend(context.env.RESEND_API_KEY);
 		const emailRequest = {
-			from: context.env.EMAIL_HELLO,
+			from: `PlanBi <${context.env.EMAIL_HELLO}>`,
 			reply_to: 'noreply@planbi.ro',
-			to: context.env.EMAIL_SEBI,
-			subject: `New Consultation Request from ${requestBody.name}`,
+			to: context.env.EMAIL_CONTACT,
+			subject: `Solicitare nouă de la ${requestBody.name}`,
 			text: messageBody,
 		}
 		const { data, error } = await resend.emails.send(emailRequest);
-		return Response.json({ req: emailRequest, data: data, error: error });
+		if(error) {
+			// TODO Log error somewhere
+			return Response.json({ req: emailRequest, data: data, error: error }, { status: 500 });
+		}
+		return Response.json({ id: data.id });
 	} catch (err) {
+		// TODO Log error somewhere
 		return Response.json({ exception: JSON.stringify(err) });
 	}
 }
