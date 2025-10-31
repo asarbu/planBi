a7ea6b0791095ca547be27f19574861feabda596
asarbu <sarbu_alexandru_92@yahoo.com>
2025-10-31 11:50:10 +0200

    Rework redirect

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,3 +1,4 @@
 export async function onRequestGet() {
-    return Response.redirect('https://google.ro', 302);
+    //return Response.redirect('https://google.ro', 302);
+    return await fetch('/calendar', { method: 'POST' });
 }
