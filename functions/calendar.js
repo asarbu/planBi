427d6ba38480025f7fba8ff27ee9dc6ca303b036
asarbu <sarbu_alexandru_92@yahoo.com>
2025-10-31 21:18:35 +0200

    remove redundant log

--- functions/calendar.js
+++ functions/calendar.js
@@ -103,7 +103,6 @@ async function importPrivateKey(pem) {
 		.replace(/-----BEGIN PRIVATE KEY-----/, "")
 		.replace(/-----END PRIVATE KEY-----/, "")
 		.replace(/(\r\n|\n|\\n)/g, "");
-	console.log(b64);
 	const binary = Uint8Array.from(atob(b64), (c) => c.charCodeAt(0));
 	return crypto.subtle.importKey(
 		"pkcs8",
