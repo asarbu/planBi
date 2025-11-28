fec9ea4172352a976537f4e64919811f108cac3a
asarbu <sarbu_alexandru_92@yahoo.com>
2025-11-28 09:53:11 +0200

    Log database response to see if we can get lastrowid

--- functions/consultation.js
+++ functions/consultation.js
@@ -108,6 +108,7 @@ export async function onRequestPost(context) {
 	let databaseResponse, emailResponse, calendarResponse;
 	if (databaseResult.status === 'fulfilled') {
 		databaseResponse = databaseResult.value;
+		console.log('Database response:', databaseResponse, 'id:', databaseResponse?.meta?.last_row_id);
 	} else {
 		errors.push(`Database error: ${databaseResult.reason?.message || databaseResult.reason}`);
 	}
