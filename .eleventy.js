8370e0859f2bc9bdd48a57971b28b566cbb257e6
asarbu@yahoo.com <sarbu_alexandru_92@yahoo.com>
2025-12-27 11:29:18 +0200

    Inject CSS and JS into HTML

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,7 @@
+const { transform } = require("lightningcss");
+const htmlmin = require("html-minifier-terser");
+const { minify } = require("terser");
+
 module.exports = function(eleventyConfig) {
   // 1. Tell 11ty to ignore the output folder
   eleventyConfig.ignores.add("_site");
@@ -5,10 +9,44 @@ module.exports = function(eleventyConfig) {
   // 2. Pass through Cloudflare-specific files/folders
   // This ensures your Functions and CF config files move to the build
   eleventyConfig.addPassthroughCopy("functions");
-  eleventyConfig.addPassthroughCopy("static");
   eleventyConfig.addPassthroughCopy("images");
 
   eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
+
+  eleventyConfig.addFilter("cssmin", function(code) {
+    const filePath = this.page ? this.page.inputPath : "inline-style.css";
+    let { code: minified } = transform({
+      filename: filePath,
+      code: Buffer.from(code, 'utf8'),
+      minify: true,
+      drafts: { nesting: true }
+    });
+    return minified.toString();
+  });
+
+  eleventyConfig.addNunjucksAsyncFilter("jsmin", async function(code, callback) {
+    try {
+      const minified = await minify(code);
+      callback(null, minified.code);
+    } catch (err) {
+      console.error("Terser error: ", err);
+      callback(null, code);
+    }
+  });
+
+  eleventyConfig.addTransform("htmlmin", function(content) {
+    if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyJS: true, // Also minifies JS inside <script> tags
+        minifyCSS: false // Also minifies CSS inside <style> tags
+      });
+      return minified;
+    }
+    return content;
+  });
 
   return {
     dir: {
