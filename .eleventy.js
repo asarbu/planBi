0561a622c0b8b2ae67c3b7498fd4c63f7af6aec2
asarbu@yahoo.com <sarbu_alexandru_92@yahoo.com>
2025-12-28 00:20:59 +0200

    Add production flag

--- .eleventy.js
+++ .eleventy.js
@@ -3,56 +3,64 @@ const htmlmin = require("html-minifier-terser");
 const { minify } = require("terser");
 
 module.exports = function(eleventyConfig) {
-  // 1. Tell 11ty to ignore the output folder
-  eleventyConfig.ignores.add("_site");
+	// 1. Tell 11ty to ignore the output folder
+	eleventyConfig.ignores.add("_site");
 
-  // 2. Pass through Cloudflare-specific files/folders
-  // This ensures your Functions and CF config files move to the build
-  eleventyConfig.addPassthroughCopy("functions");
-  eleventyConfig.addPassthroughCopy("images");
+	// 2. Pass through Cloudflare-specific files/folders
+	// This ensures your Functions and CF config files move to the build
+	eleventyConfig.addPassthroughCopy("functions");
+	eleventyConfig.addPassthroughCopy("images");
 
-  eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
+	eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
 
-  eleventyConfig.addFilter("cssmin", function(code) {
-    const filePath = this.page ? this.page.inputPath : "inline-style.css";
-    let { code: minified } = transform({
-      filename: filePath,
-      code: Buffer.from(code, 'utf8'),
-      minify: true,
-      drafts: { nesting: true }
-    });
-    return minified.toString();
-  });
+	const isProduction = process.env.NODE_ENV === "production";
+	eleventyConfig.addFilter("cssmin", function(code) {
+		if (!isProduction) { return code; }
 
-  eleventyConfig.addNunjucksAsyncFilter("jsmin", async function(code, callback) {
-    try {
-      const minified = await minify(code);
-      callback(null, minified.code);
-    } catch (err) {
-      console.error("Terser error: ", err);
-      callback(null, code);
-    }
-  });
+		const filePath = this.page ? this.page.inputPath : "inline-style.css";
+		let { code: minified } = transform({
+			filename: filePath,
+			code: Buffer.from(code, 'utf8'),
+			minify: true,
+			drafts: { nesting: true }
+		});
+		return minified.toString();
+	});
 
-  eleventyConfig.addTransform("htmlmin", function(content) {
-    if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-        minifyJS: true, // Also minifies JS inside <script> tags
-        minifyCSS: false // Also minifies CSS inside <style> tags
-      });
-      return minified;
-    }
-    return content;
-  });
+	eleventyConfig.addNunjucksAsyncFilter("jsmin", async function(code, callback) {
+		try {
+			if (!isProduction) { return callback(null, code); }
 
-  return {
-    dir: {
-      input: "src",
-      output: "_site",   // Standard output for Cloudflare to pick up
-      includes: "_includes" // Optional: where you keep layouts
-    }
-  };
+			const minified = await minify(code, {
+				compress: {
+					drop_console: true
+				}
+			});
+			callback(null, minified.code);
+		} catch (err) {
+			console.error("Terser error: ", err);
+			callback(null, code);
+		}
+	});
+
+	eleventyConfig.addTransform("htmlmin", function(content) {
+		if (!isProduction) { return content; }
+		if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true
+			});
+			return minified;
+		}
+		return content;
+	});
+
+	return {
+		dir: {
+			input: "src",
+			output: "_site",   // Standard output for Cloudflare to pick up
+			includes: "_includes" // Optional: where you keep layouts
+		}
+	};
 };
